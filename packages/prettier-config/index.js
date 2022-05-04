53811848261f03894bc8dfa26b5963d3e92d261c
nghiatv <nghiait0111@gmail.com>
2022-05-04 15:51:53 +0700

    update singleQuote from true to false

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     printWidth: 120,
     tabWidth: 2,
     arrowParens: 'always',
-    singleQuote: true,
+    singleQuote: false,
     semi: true,
     trailingComma: 'es5',
     bracketSpacing: true,
