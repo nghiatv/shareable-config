2afd32b88b5d22bd2b320a7999a06771d03a7bcd
nghiatv <nghiait0111@gmail.com>
2022-05-04 15:31:24 +0700

    fix postinstall not working

--- packages/prettier-config/scripts/postinstall.js
+++ packages/prettier-config/scripts/postinstall.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const filePath = path.join(process.env.INIT_CWD, '.prettierrc.json')
+const filePath = path.join(process.env.INIT_CWD, '.prettierrc.js');
 
 if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, `module.exports = "@nghiatv/prettier-config"`);
